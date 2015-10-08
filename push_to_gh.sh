0290a86a754d0624b52f623fb107922f04025031
Ken Struys <ken@struys.ca>
2015-10-07 21:09:13 -0700

    fix push_to_gh.sh derp

--- push_to_gh.sh
+++ push_to_gh.sh
@@ -2,17 +2,17 @@
 
 set -ex
 
-[ "$TRAVIS_BRANCH" == "real_master" -a "$TRAVIS_PULL_REQUEST" == "false" ] || exit
+[ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ] || exit
 
 git clone "https://${GH_TOKEN}@${GH_REF}" out >& /dev/null
 cd out
-git checkout master
+git checkout gh-pages
 git config user.name "Travis-CI"
 git config user.email "ken@struys.ca"
 # Repo
 cp ../.travis.yml .
 # Website
-cp -R ../build ../bower_components ../index.html ../*.png ../favicon.ico .
+cp -R ../build ../bower_components ../index.htm ../assets .
 
 git add .
 git commit -m "Deployed ${TRAVIS_BUILD_NUMBER} to Github Pages"
